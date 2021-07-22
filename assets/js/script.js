3945ca65349a4c4576eeffacde183873719f4ca7
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2021-07-22 11:51:56 -0300

    likes mudam de cor

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,19 @@ $(function() {
    $('[data-toggle="tooltip"]').tooltip();
    $('[data-toggle="popover"]').popover();
 })
+
+function setLike( x ){
+   
+   let og , swp;
+
+   og  = "fa";
+   swp = "far"
+   if ( x.classList.contains( swp ) ){
+      og = "far";
+      swp = "fa";
+   }
+
+   x.classList.remove( og );
+   x.classList.add( swp );
+
+}
