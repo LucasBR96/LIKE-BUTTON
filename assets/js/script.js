e5c4650e1e5de394d3d3c04d13982616d12acb42
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2021-07-22 16:47:50 -0300

    comportamento esperado das cores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,52 @@
 $(function() {
    $('[data-toggle="tooltip"]').tooltip();
    $('[data-toggle="popover"]').popover();
-})
+});
 
-function setLike( x ){
+var likeDict = {};
+function getId( nome ){
+
+   if( !likeDict[ nome ] ){
+      likeDict[ nome ] = [ false , false ];
+   }return likeDict[ nome ]
+}
+
+function setById( nome , val ){
+   likeDict[ nome ] = val;
+}
+
+function manageLike( src , nome ){
+
+   let vals = getId( nome );
+   let x1 = vals[ 0 ];
+   let x2 = vals[ 1 ];
+
+   vals[ 0 ] = ( src&& ( !x1 ) );
+   vals[ 1 ] = ( ( !src) && ( !x2 ));
+}
+
+function setLike( num , x ){
    
-   let og , swp;
+   let nome = x.parentElement.id;
+   let bool_val = ( num == 1 );
 
-   og  = "fa";
-   swp = "far"
-   if ( x.classList.contains( swp ) ){
-      og = "far";
-      swp = "fa";
+   manageLike( bool_val , nome );
+   let val = likeDict[ nome ];
+   //console.log( bool_val , nome , val );
+
+   let child, tag, old;
+   for( let i = 0 ; i < 2 ; i++ ){
+
+      child = x.parentElement.children[ i ];
+      tag = "far";
+      old = "fa"
+      if ( val[ i ] ){
+         tag = "fa";
+         old = "far";
+      }
+
+      child.classList.remove( old )
+      child.classList.add( tag );
    }
-
-   x.classList.remove( og );
-   x.classList.add( swp );
-
+   
 }
